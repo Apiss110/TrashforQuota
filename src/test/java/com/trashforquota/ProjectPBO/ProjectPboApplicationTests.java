c878ca2e1deec6573024e66883fcd530f26c1cf4
Hafidz <hafidz.al@mhs.itenas.ac.id>
2026-05-05 23:29:59 +0700

    first commit

---
+++ src/test/java/com/trashforquota/ProjectPBO/ProjectPboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trashforquota.ProjectPBO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
